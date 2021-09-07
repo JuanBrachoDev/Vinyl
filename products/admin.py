f27173e63f7a7e5bbc6c254d4a4dc3ee7c81edb1
JuanBrachoDev <juanjobracho@gmail.com>
2021-09-07 17:32:33 +0000

    Created admin classes for products and registered the products models.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Album, Artist
 
 # Register your models here.
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'artist',
+        'genre',
+        'price',
+        'rating',
+        'spotify_link',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'image'
+    )
+
+    ordering = ('name',)
+
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Artist, ArtistAdmin)
