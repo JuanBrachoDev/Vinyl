e4e3bd0619ac63e959651308cf0c7ff7cb33d498
JuanBrachoDev <juanjobracho@gmail.com>
2021-09-07 14:52:07 +0000

    Added products models for artists and albums, installed pillow and updated requirements, migrated products.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Artists'
+
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Albums'
+
+    artist = models.ForeignKey('Artist', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    genre = models.CharField(max_length=254)
+    spotify_link = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
