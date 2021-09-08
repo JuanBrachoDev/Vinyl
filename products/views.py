7ce73ee7aade4a89970cd8ede31e1c8fb3e822c3
JuanBrachoDev <juanjobracho@gmail.com>
2021-09-08 20:14:30 +0000

    Added base view for albums, added test images for albums and artists in database, added basic albums template.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Album
 
 # Create your views here.
+
+def all_albums(request):
+    """ A view to show all albums, including sorting and search queries """
+
+    albums = Album.objects.all()
+
+    context = {
+        'albums': albums,
+    }
+
+    return render(request, 'products/albums.html', context)
