7c0496175ac32684d5d91df8b12d3aec1f15bde4
JuanBrachoDev <juanjobracho@gmail.com>
2021-09-14 10:19:29 +0000

    Added basic search functionality to albums view, added GET actions to base and mobile headers.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Album, Artist
 
 # Create your views here.
@@ -8,9 +10,21 @@ def all_albums(request):
     """ A view to show all albums, including sorting and search queries """
 
     albums = Album.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('albums'))
+
+            queries = Q(name__icontains=query) | Q(genre__icontains=query) | Q(artist__name__icontains=query)
+            albums = albums.filter(queries)
 
     context = {
         'albums': albums,
+        'search_term': query,
     }
 
     return render(request, 'products/albums.html', context)
