633ab294d6f4a07b19b44f9bb17324bc878cd9ce
JuanBrachoDev <juanjobracho@gmail.com>
2021-09-17 19:00:49 +0000

    Added toasts to all outcomes in cart views, modified success toast so it shows cart details to the user.

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Album
 
@@ -14,13 +14,14 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
-    album = Album.objects.get(pk=item_id)
+    album = get_object_or_404(Album, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {album.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
         messages.success(request, f'Added {album.name} to your cart.')
@@ -32,13 +33,16 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    album = get_object_or_404(Album, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated {album.name} quantity to {cart[item_id]}')
     else:
         cart.pop(item_id)
+        messages.success(request, f'Removed {album.name} from your cart.')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -47,13 +51,15 @@ def adjust_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """Remove the item from the shopping cart"""
 
-    cart = request.session.get('cart', {})
-
     try:
+        cart = request.session.get('cart', {})
+        album = get_object_or_404(Album, pk=item_id)
         cart.pop(item_id)
+        messages.success(request, f'Removed {album.name} from your cart.')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
