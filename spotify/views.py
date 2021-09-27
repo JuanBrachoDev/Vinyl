2b145b3388ece7e22b2f2c78d08fa2fa20f269ad
JuanBrachoDev <juanjobracho@gmail.com>
2021-09-27 15:28:34 +0000

    Added spotify authorize url view, added spotify basic urls, installed djangorestframework, added spotify settings.

--- spotify/views.py
+++ spotify/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from rest_framework.views import APIView
+from requests import Request, post
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+
+# A view that returns the url request for spotify to authorize
+class AuthURL(APIView):
+    def get(self, request, fornat=None):
+        scopes = 'user-read-email'
+
+        url = Request('GET', 'https://accounts.spotify.com/authorize', params={
+            'scope': scopes,
+            'response_type': 'code',
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID
+        }).prepare().url
+
+        return Response({'url': url}, status=status.HTTP_200_OK)
