8739ad246f6a6d587f22b8c9d81df81bf507d2fb
JuanBrachoDev <juanjobracho@gmail.com>
2021-09-29 04:00:35 +0000

    Changed account auth method to username_email, added links to profile page, changed str method for profile model to use usernames, added profile variable to profile view.

--- profiles/models.py
+++ profiles/models.py
@@ -17,7 +17,7 @@ class UserProfile(models.Model):
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_eircode = models.CharField(max_length=20, null=True, blank=True)
     def __str__(self):
-        return self.user.email
+        return self.user.username
 
 
 @receiver(post_save, sender=User)
