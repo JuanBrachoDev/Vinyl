e8ea1e622d7c6a45592e50c9a6ded609d7860412
JuanBrachoDev <juanjobracho@gmail.com>
2021-10-02 02:20:12 +0000

    Added basic artist and album forms.

---
+++ products/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Album, Artist
+
+
+class AlbumForm(forms.ModelForm):
+
+    class Meta:
+        model = Album
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+
+
+class ArtistForm(forms.ModelForm):
+
+    class Meta:
+        model = Artist
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
