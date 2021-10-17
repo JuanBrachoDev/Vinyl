1886e4a99ce12c73b90a814f8df3216d930dd30f
JuanBrachoDev <juanjobracho@gmail.com>
2021-10-17 19:57:13 +0000

    Formatted and commented all files in home app, added comments to checkout.css and changed payment overlay colour.

--- home/views.py
+++ home/views.py
@@ -6,30 +6,38 @@ from products.models import Album, Artist
 
 # A view that shows the landing page
 def index(request):
-    """ A view to return the index page with some of the special
-    deals and a list of all albums at the end. """
+    """A view to return the index page with some of the special
+    deals and a list of all albums at the end."""
 
     all_albums = Album.objects.all()
-    albums = all_albums.order_by('name')
+    albums = all_albums.order_by("name")
 
+    # Featured Artist section
     all_featured_artists = Artist.objects.filter(is_featured_artist=True)
-    featured_artist_id = all_featured_artists.order_by('?')[:1][0].id
+    featured_artist_id = all_featured_artists.order_by("?")[:1][0].id
     featured_artist = all_featured_artists.filter(id=featured_artist_id)
-    featured_artist_albums = all_albums.filter(artist=featured_artist_id).order_by('?')[:4]
+    featured_artist_albums = all_albums.filter(artist=featured_artist_id).order_by("?")[
+        :4
+    ]
+  
+    # New Arrivals section
+    new_arrivals = all_albums.filter(special_offer_category="new_arrival").order_by(
+        "?"
+    )[:2]
 
-    new_arrivals = all_albums.filter(special_offer_category='new_arrival').order_by('?')[:2]
+    # Latest Deals section
+    deals = all_albums.filter(special_offer_category="deal").order_by("?")[:4]
 
-    deals = all_albums.filter(special_offer_category='deal').order_by('?')[:4]
-
-    clearances = all_albums.filter(special_offer_category='clearance').order_by('?')[:2]
+    # Clearance section
+    clearances = all_albums.filter(special_offer_category="clearance").order_by("?")[:2]
 
     context = {
-        'albums': albums,
-        'featured_artist': featured_artist,
-        'featured_artist_albums': featured_artist_albums,
-        'new_arrivals': new_arrivals,
-        'deals': deals,
-        'clearances': clearances,
+        "albums": albums,
+        "featured_artist": featured_artist,
+        "featured_artist_albums": featured_artist_albums,
+        "new_arrivals": new_arrivals,
+        "deals": deals,
+        "clearances": clearances,
     }
 
-    return render(request, 'home/index.html', context)
+    return render(request, "home/index.html", context)
